c3a3b9ee7d24a8fa702132770dc587b518b7a706
cyberloo <96814415+cyberloo@users.noreply.github.com>
2022-10-13 15:43:24 -0400

    Revert "packages + @syncfusion imports"
    This reverts commit 252abfd45adef1da3409a8483ca7d3e70ed9dec2.

--- front-end/src/app/cars/cars.component.ts
+++ front-end/src/app/cars/cars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Car } from '../models/cars';
 import { Form } from '@angular/forms';
 import { HttpService } from '../services/http-services/http-service.service';
